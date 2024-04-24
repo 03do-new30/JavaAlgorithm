fa0600a014f7045a4909116e554b6743d5511c1f
donew <eastgloss0330@naver.com>
2024-04-24 15:49:49 +0900

    [프로그래머스] 폰켓몬

---
+++ src/Programmers/코딩테스트고득점kit/PGM_폰켓몬_1845.java
@@ -0,0 +1,29 @@
+package Programmers.코딩테스트고득점kit;
+
+import java.util.HashSet;
+
+public class PGM_폰켓몬_1845 {
+    public static void main(String[] args) {
+        int[][] nums = {{3, 1, 2, 3}, {3, 3, 3, 2, 2, 4}, {3, 3, 3, 2, 2, 2,}};
+        int[] result = {2, 3, 2};
+        Solution sol = new Solution();
+        for (int i = 0; i < result.length; i++) {
+            System.out.println("# " + i);
+            System.out.println(sol.solution(nums[i]) == result[i]);
+        }
+    }
+
+    static class Solution {
+        public int solution(int[] nums) {
+            int answer = 0;
+            int pick = nums.length / 2;
+            HashSet<Integer> hashSet = new HashSet<>();
+            for (int n: nums) {
+                hashSet.add(n);
+            }
+            if (pick >= hashSet.size()) answer = hashSet.size();
+            else answer = pick;
+            return answer;
+        }
+    }
+}
