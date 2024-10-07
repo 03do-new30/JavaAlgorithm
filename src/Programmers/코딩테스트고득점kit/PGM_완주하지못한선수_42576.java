677babefefc3aed5714117a3259302351f2ee36d
donew <eastgloss0330@naver.com>
2024-10-07 18:13:41 +0900

    [프로그래머스] 완주하지 못한 선수

--- src/Programmers/코딩테스트고득점kit/PGM_완주하지못한선수_42576.java
+++ src/Programmers/코딩테스트고득점kit/PGM_완주하지못한선수_42576.java
@@ -1,8 +1,6 @@
 package Programmers.코딩테스트고득점kit;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class PGM_완주하지못한선수_42576 {
     public static void main(String[] args) {
@@ -25,23 +23,19 @@ public class PGM_완주하지못한선수_42576 {
     static class Solution {
         public String solution(String[] participant, String[] completion) {
             String answer = "";
-            HashMap<String, Integer> map = new HashMap<>();
-            for (String p : participant) {
-                // HashMap.getOrDefault('A', 0)
-                // A에 해당하는 value가 있으면 가져오고 아닐 경우 default를 0으로
-                map.put(p, map.getOrDefault(p, 0) + 1);
+
+            HashMap<String, Integer> participants = new HashMap<>();
+            for (String name : participant) {
+                participants.put(name, participants.getOrDefault(name, 0) + 1);
             }
 
-            for (String p : completion) {
-                map.put(p, map.get(p) - 1);
+            for (String name : completion) {
+                participants.replace(name, participants.get(name) - 1);
             }
 
-            // Iterator 사용법 숙지하기!
-            Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-            while (iterator.hasNext()) {
-                Map.Entry<String, Integer> entry = iterator.next();
-                if (entry.getValue() == 1) {
-                    answer = entry.getKey();
+            for (String name : participants.keySet()) {
+                if (participants.get(name) > 0) {
+                    answer = name;
                     break;
                 }
             }
