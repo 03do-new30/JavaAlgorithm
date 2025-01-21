eb1e0fcabf5c921406b5e0d0e236270b303a1ee0
Donew <eastgloss0330@naver.com>
2025-01-21 22:51:05 +0900

    Reformat file

--- src/SEA/SEA_1204_최빈수_구하기.java
+++ src/SEA/SEA_1204_최빈수_구하기.java
@@ -5,43 +5,43 @@ import java.io.*;
 
 public class SEA_1204_최빈수_구하기 {
 
-        private static final int N = 1000;
+    private static final int N = 1000;
 
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-            int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-            for (int test_case = 1; test_case <= T; test_case++) {
-                int tc = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int tc = Integer.parseInt(br.readLine());
 
-                int[] scores = new int[N];
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int i = 0; i < N; i++) {
-                    scores[i] = Integer.parseInt(st.nextToken());
-                }
-
-                int[] count = new int[101];
-                for (int score : scores) {
-                    count[score]++;
-                }
-
-                int freqScore = 0;
-                int freqCnt = 0;
-                for (int i = 0; i <= 100; i++) {
-                    if (freqCnt <= count[i]) {
-                        freqScore = i;
-                        freqCnt = count[i];
-                    }
-                }
-
-                bw.write("#" + tc + " " + freqScore + "\n");
+            int[] scores = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                scores[i] = Integer.parseInt(st.nextToken());
             }
 
-            br.close();
-            bw.flush();
-            bw.close();
+            int[] count = new int[101];
+            for (int score : scores) {
+                count[score]++;
+            }
+
+            int freqScore = 0;
+            int freqCnt = 0;
+            for (int i = 0; i <= 100; i++) {
+                if (freqCnt <= count[i]) {
+                    freqScore = i;
+                    freqCnt = count[i];
+                }
+            }
+
+            bw.write("#" + tc + " " + freqScore + "\n");
         }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
--- src/SEA/SEA_2001_파리퇴치.java
+++ src/SEA/SEA_2001_파리퇴치.java
@@ -17,11 +17,11 @@ public class SEA_2001_파리퇴치 {
             int m = Integer.parseInt(st.nextToken());
 
             // 누적합을 만들기 위해 위쪽, 왼쪽 모서리를 0으로 처리해준다.
-            int[][] arr = new int[n+1][n+1];
+            int[][] arr = new int[n + 1][n + 1];
 
-            for (int i = 1; i < n+1; i++) {
+            for (int i = 1; i < n + 1; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 1; j < n+1; j++) {
+                for (int j = 1; j < n + 1; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
@@ -31,15 +31,15 @@ public class SEA_2001_파리퇴치 {
 
             // 누적합을 통해 파리채로 잡을 수 있는 파리의 최대 마리수를 구한다.
             int answer = 0;
-            for (int r = 1; r < n+1; r++) {
-                for (int c = 1; c < n+1; c++) {
+            for (int r = 1; r < n + 1; r++) {
+                for (int c = 1; c < n + 1; c++) {
                     int nr = r + m - 1;
                     int nc = c + m - 1;
-                    if (nr < 0 || nr >= n+1 || nc < 0 || nc >= n+1) {
+                    if (nr < 0 || nr >= n + 1 || nc < 0 || nc >= n + 1) {
                         continue;
                     }
                     // 파리채로 잡을 수 있는 구간의 파리 마릿수 구하기
-                    int tmp = prefixSum[nr][nc] - prefixSum[r-1][nc] - prefixSum[nr][c-1] + prefixSum[r-1][c-1];
+                    int tmp = prefixSum[nr][nc] - prefixSum[r - 1][nc] - prefixSum[nr][c - 1] + prefixSum[r - 1][c - 1];
                     if (answer < tmp) {
                         answer = tmp;
                     }
@@ -62,7 +62,7 @@ public class SEA_2001_파리퇴치 {
 
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < n; j++) {
-                prefixSum[i][j] = prefixSum[i][j-1] + prefixSum[i-1][j] - prefixSum[i-1][j-1] + arr[i][j];
+                prefixSum[i][j] = prefixSum[i][j - 1] + prefixSum[i - 1][j] - prefixSum[i - 1][j - 1] + arr[i][j];
             }
         }
 
